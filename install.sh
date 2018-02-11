96117a1bfb4296c18f248ae0fc02a8ef1072f6cc
sgrigorov <s_grigorov@yahoo.com>
2018-02-11 13:42:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,10 @@ touch /tmp/install2
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Ubuntu > /dev/null && touch /tmp/install.ubuntu.sh
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Fedora > /dev/null && {
   touch /tmp/install.fedora1.sh
+
+
+
+
+
+
 }
