7f2c150740d03df80fdd986bce8ba11e9a905127
sgrigorov <s_grigorov@yahoo.com>
2018-02-12 12:23:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,9 @@ grep \^ID= /etc/os-release | grep "centos" > /dev/null && {
   rpm -ivh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
   yum install git puppet rubygems -y
   gem install r10k
-  
-  
-  
-  
 }
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Ubuntu > /dev/null && {
+  #touch /tmp/install.ubuntu.sh
   touch /tmp/install.ubuntu.sh
   apt-get install puppet r10k git
 }
