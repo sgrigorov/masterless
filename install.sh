73c11aba4036e8c6a1a246bb3929caeab84f71e8
sgrigorov <s_grigorov@yahoo.com>
2018-02-12 12:21:14 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 touch /tmp/install2
+grep \^ID= /etc/os-release | grep "centos" > /dev/null && {
+  rpm -ivh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
+  yum install git puppet rubygems -y
+  gem install r10k
+}
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Ubuntu > /dev/null && {
   touch /tmp/install.ubuntu.sh
+  #apt-get install puppet r10k git
 }
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Fedora > /dev/null && {
   touch /tmp/install.fedora1.sh
@@ -8,8 +14,4 @@ lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Fedora > /dev/null && {
     dnf install puppet git -y
   }
 }
-grep \^ID= /etc/os-release | grep "centos" > /dev/null && {
-  rpm -ivh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-  yum install git puppet rubygems -y
-  gem install r10k
-}
+
