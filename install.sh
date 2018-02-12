364612628171d953806ec8859898b509ed31d54c
sgrigorov <s_grigorov@yahoo.com>
2018-02-11 19:54:52 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Ubuntu > /dev/null && {
 }
 lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Fedora > /dev/null && {
   touch /tmp/install.fedora1.sh
-  rpm -qa puppet | sed 's/-.*//' | grep puppet > /dev/null) || {
+  rpm -qa puppet | sed 's/-.*//' | grep puppet > /dev/null || {
     dnf install puppet -y
   }
 }
