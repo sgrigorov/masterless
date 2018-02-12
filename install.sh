acd3b7c2e64d15de5fe7436f4448d9aea21c28a0
sgrigorov <s_grigorov@yahoo.com>
2018-02-11 23:32:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ lsb_release -i | sed 's/^Distributor ID:\t// ' | grep Fedora > /dev/null && {
   touch /tmp/install.fedora1.sh
   rpm -qa puppet | sed 's/-.*//' | grep puppet > /dev/null || {
     dnf install puppet git -y
-    
-    
-    
   }
+}
+grep \^ID= /etc/os-release | grep "centos" > /dev/null && {
+  yum install git puppet -y
 }
