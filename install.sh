a5a3a6fc8985e7fe1410eef13ecb32d464db3a10
sgrigorov <s_grigorov@yahoo.com>
2018-02-13 21:37:42 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,8 +22,9 @@ case $test_os_id in
     echo "OS - CentOS"
     puppet_dir=puppetlabs
     rpm -ivh https://yum.puppetlabs.com/puppet5/puppet5-release-el-7.noarch.rpm
-    yum install git puppet vim rubygems -y
-    gem install r10k
+    yum install git puppet vim -y
+    sh /etc/profile.d/puppet-agent.sh
+    /opt/puppetlabs/puppet/bin/gem install r10k
   ;;
   fedora)
     echo "OS - Fedora"
@@ -39,4 +40,5 @@ esac
 
 mkdir /etc/$puppet_dir/r10k
 curl https://raw.githubusercontent.com/sgrigorov/masterless/production/files/$puppet_dir/r10k.yaml > /etc/$puppet_dir/r10k/r10k.yaml
-/usr/local/bin/r10k deploy environment -p -c /etc/$puppet_dir/r10k/r10k.yaml
+#/usr/local/bin/r10k deploy environment -p -c /etc/$puppet_dir/r10k/r10k.yaml
+r10k deploy environment -p -c /etc/$puppet_dir/r10k/r10k.yaml
