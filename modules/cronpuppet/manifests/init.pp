76a19021fa20550379d54753d187b54afd59adbf
sgrigorov <s_grigorov@yahoo.com>
2018-02-15 10:45:12 -0700

    Update init.pp

--- modules/cronpuppet/manifests/init.pp
+++ modules/cronpuppet/manifests/init.pp
@@ -1,3 +1,8 @@
 class cronpuppet {
-
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "puppet apply  /etc/puppetlabs/code/environments/production/manifests/site.pp",
+        user    => root,
+        minute  => '*/30',
+    }
 }
