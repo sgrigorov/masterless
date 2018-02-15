a6e09162cf93e1d92757fe9d762103fd6324c851
sgrigorov <s_grigorov@yahoo.com>
2018-02-15 10:32:14 -0700

    Update init.pp

--- modules/cron-puppet/manifests/init.pp
+++ modules/cron-puppet/manifests/init.pp
@@ -1,17 +1,8 @@
 class cron-puppet {
-    file { 'post-hook':
-        ensure  => file,
-        path    => '/etc/puppet/.git/hooks/post-merge',
-        source  => 'puppet:///modules/cron-puppet/post-merge',
-        mode    => 0755,
-        owner   => root,
-        group   => root,
-    }
     cron { 'puppet-apply':
         ensure  => present,
-        command => "cd /etc/puppet ; /usr/bin/git pull",
+        command => "puppet apply  /etc/puppetlabs/code/environments/production/manifests/site.pp",
         user    => root,
         minute  => '*/30',
-        require => File['post-hook'],
     }
 }
