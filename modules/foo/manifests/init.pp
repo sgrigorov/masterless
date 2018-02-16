c54e3f64395679466c98b30f5530ae92130ac9e0
sgrigorov <s_grigorov@yahoo.com>
2018-02-16 15:12:55 -0700

    Update init.pp

--- modules/foo/manifests/init.pp
+++ modules/foo/manifests/init.pp
@@ -7,11 +7,11 @@ class foo {
   #Where $module_stdlib is a module I whant to install. 
   #The /bin path is a path where the grep comes from. 
   #And the /opt/puppetlabs/bin is a path where for the puppet binary exists in my installation.
-  $module_stdlib = 'puppetlabs-stdlib'
-  #exec { 'puppet_module_stdlib':
-    #command => "puppet module install ${module_stdlib}",
-    #unless  => "puppet module list | grep ${module_stdlib}",
-    #path    => ['/bin', '/opt/puppetlabs/bin'],
-    #user    => "root",
-  #}
+  $module_stdlib = 'puppet-nginx'
+  exec { 'puppet_module_nginx':
+    command => "puppet module install ${module_stdlib}",
+    unless  => "puppet module list | grep ${module_stdlib}",
+    path    => ['/bin', '/opt/puppetlabs/bin'],
+    user    => "root",
+  }
 }
