f564ff170ce8c9a17374f8cb8b016ffe2cb4dee7
sgrigorov <s_grigorov@yahoo.com>
2018-02-17 11:37:32 -0700

    Delete init1.pp

--- modules/init1.pp
+++
@@ -1,8 +0,0 @@
-class cron-puppet {
-    cron { 'puppet-apply':
-        ensure  => present,
-        command => "puppet apply  /etc/puppetlabs/code/environments/production/manifests/site.pp",
-        user    => root,
-        minute  => '*/30',
-    }
-}
