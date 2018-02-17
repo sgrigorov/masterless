6b1fae331b89128d76f404a336b52a46030352a4
sgrigorov <s_grigorov@yahoo.com>
2018-02-16 17:46:41 -0700

    Update init.pp

--- modules/foo/manifests/init.pp
+++ modules/foo/manifests/init.pp
@@ -4,13 +4,14 @@ class foo {
       ensure => file,
       source => "puppet:///modules/foo/hello";
   }
-  #Where $module_stdlib is a module I whant to install. 
-  #The /bin path is a path where the grep comes from. 
+  #Where $module_stdlib is a module I whant to install.
+  #The /bin path is a path where the grep comes from.
   #And the /opt/puppetlabs/bin is a path where for the puppet binary exists in my installation.
   $module_stdlib = 'puppet-nginx'
   exec { 'puppet_module_nginx':
-    command => "/opt/puppetlabs/bin/puppet module install puppet-nginx",
-    unless  => "/opt/puppetlabs/bin/puppet module list | grep puppet-nginx",
-    path    => '/bin:/opt/puppetlabs/bin',
+        path    => [ '/opt/puppetlabs/bin/', '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+        command => "puppet module install puppet-nginx",
+        unless  => "/opt/puppetlabs/bin/puppet module list | /bin/grep puppet-nginx",
   }
+}
 }
