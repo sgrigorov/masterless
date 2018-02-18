9d558ffc7a92a2fc7e982e14074ab39a78ff742f
Stan Grigorov <s_grigorov@yahoo.com>
2018-02-18 15:32:05 -0700

    a

--- modules/install/fedora.pp
+++ modules/install/fedora.pp
@@ -1,12 +1,16 @@
 class examplerpm ( $src ) {
 
-  package { 'rpmfusion-free-release-$facts['os']['release']['full'].noarch':
-     provider => 'rpm',
-     ensure => present,
-     source => "${fusionfree::src}",
- }
-}
+  # Install Fedora free Rpmfusion
+  package { "rpmfusion-free-release-$facts['os']['release']['full'].noarch":
+    provider => 'rpm',
+    ensure => present,
+    source => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$facts['os']['release']['full'].noarch.rpm",
+  }
 
-class { 'fusionfree':
-  src => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$facts['os']['release']['full'].noarch.rpm",
+  # Install Fedora fnonree Rpmfusion
+  package { "rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch":
+    provider => 'rpm',
+    ensure => present,
+    source => "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch.rpm",
+  }
 }
