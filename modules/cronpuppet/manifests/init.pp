d2e865ffa595556cfa67e56655a8e1206c417cb2
sgrigorov <s_grigorov@yahoo.com>
2018-02-18 09:41:02 -0700

    Update init.pp

--- modules/cronpuppet/manifests/init.pp
+++ modules/cronpuppet/manifests/init.pp
@@ -2,12 +2,15 @@ class cronpuppet {
     cron { 'r10k-deploy':
         ensure  => present,
         command => "r10k deploy environment -p -c /etc/puppetlabs/r10k/r10k.yaml",
+        environment => [ 'PATH="/opt/puppetlabs/bin/"' ],
+        #path    => '/opt/puppetlabs/bin/:/bin/:/sbin/:/usr/bin/:/usr/sbin/',
         user    => root,
         minute  => [1, 31],
     }
     cron { 'puppet-apply':
         ensure  => present,
         command => "puppet apply  /etc/puppetlabs/code/environments/production/manifests/site.pp",
+        environment => [ 'PATH="/opt/puppetlabs/bin/"' ],
         user    => root,
         minute  => [5, 35],
     }
