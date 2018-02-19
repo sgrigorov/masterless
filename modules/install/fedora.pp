78004288bff2d14a1acd3812caff9a76ccfa3d4e
sgrigorov <s_grigorov@yahoo.com>
2018-02-18 18:30:38 -0700

    Update fedora.pp

--- modules/install/fedora.pp
+++ modules/install/fedora.pp
@@ -14,3 +14,16 @@ class fedorarpmfsion {
     source => "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch.rpm",
   }
 }
+
+# RedHat and Centos - http://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm
+# RedHat and Centos - http://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
+
+# Class: gnometools
+# Installs Gnome Tools
+#
+class gnometools {
+  # Gnome Tweak Tool
+  package { "gnome-tweak-tool"
+    ensure => present,
+  }
+}
