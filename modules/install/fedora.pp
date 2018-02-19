adb4c93c6ce7ae771f7784038e9af5ff59a4eea5
sgrigorov <s_grigorov@yahoo.com>
2018-02-18 18:55:09 -0700

    Update fedora.pp

--- modules/install/fedora.pp
+++ modules/install/fedora.pp
@@ -23,7 +23,7 @@ class fedorarpmfsion {
 #
 class gnometools {
   # Gnome Tweak Tool
-  package { "gnome-tweak-tool"
-    ensure => present,
-  }
+  package { "gnome-tweak-tool": ensure => present, }
+  package { "dconf":            ensure => present, }
+  package { "vlc-core":         ensure => present, }
 }
