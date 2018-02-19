6d5c1b8ceac96b69f90e4a3059ed2a359c038894
sgrigorov <s_grigorov@yahoo.com>
2018-02-19 10:14:50 -0700

    Update centosrpmfusion.pp

--- modules/install/manifests/centosrpmfusion.pp
+++ modules/install/manifests/centosrpmfusion.pp
@@ -2,17 +2,21 @@
 # RedHat and Centos - http://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm
 class install::centosrpmfusion {
 
+  $osrelease = $facts['os']['release']['full']
+
   # Install CentOS free Rpmfusion
-  package { "rpmfusion-free-release-$facts['os']['release']['full'].noarch":
-    provider => 'rpm',
-    ensure => present,
-    source => "https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$facts['os']['release']['full'].noarch.rpm",
+  package { "centos-rpmfusion-free":
+    name      => "rpmfusion-free-release-$osrelease.noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$osrelease.noarch.rpm",
   }
 
   # Install CentOS nonfree Rpmfusion
-  package { "rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch":
-    provider => 'rpm',
-    ensure => present,
-    source => "https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch.rpm",
+  package { "centos-rpmfusion-nonfree":
+    name      => "rpmfusion-nonfree-release-$osrelease.noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$osrelease.noarch.rpm",
   }
 }
