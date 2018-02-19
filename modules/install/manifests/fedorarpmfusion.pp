543326a5c5ef1ca57e5398d0c480d79cd8ecb92d
sgrigorov <s_grigorov@yahoo.com>
2018-02-19 09:42:29 -0700

    Update fedorarpmfusion.pp

--- modules/install/manifests/fedorarpmfusion.pp
+++ modules/install/manifests/fedorarpmfusion.pp
@@ -1,16 +1,18 @@
 class install::fedorarpmfusion {
 
   # Install Fedora free Rpmfusion
-  package { "rpmfusion-free-release-$facts['os']['release']['full'].noarch":
-    provider => 'rpm',
-    ensure => present,
-    source => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$facts['os']['release']['full'].noarch.rpm",
+  package { "fedora-rpmfusion-free":
+    name      => "rpmfusion-free-release-$facts['os']['release']['full'].noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$facts['os']['release']['full'].noarch.rpm",
   }
 
   # Install Fedora fnonree Rpmfusion
-  package { "rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch":
-    provider => 'rpm',
-    ensure => present,
-    source => "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch.rpm",
+  package { "fedora-rpmfusion-nonfree":
+    name      => "rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$facts['os']['release']['full'].noarch.rpm",
   }
 }
