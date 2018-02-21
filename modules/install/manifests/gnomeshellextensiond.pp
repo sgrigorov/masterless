336f814aee1ba7b1f93be3ae1d0b259377866244
sgrigorov <s_grigorov@yahoo.com>
2018-02-20 22:38:56 -0700

    Create gnomeshellextensiond.pp

---
+++ modules/install/manifests/gnomeshellextensiond.pp
@@ -0,0 +1,2 @@
+#https://github.com/brunelli/gnome-shell-extension-installer/blob/master/gnome-shell-extension-installer
+#http://bernaerts.dyndns.org/linux/76-gnome/283-gnome-shell-install-extension-command-line-script
