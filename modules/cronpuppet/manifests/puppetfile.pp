ddb115bd460fa620ddda39709619e6a21e00e9ae
Stan <s_grigorov@yahoo.com>
2018-02-23 08:59:22 -0700

    Update puppetfile.pp

--- modules/cronpuppet/manifests/puppetfile.pp
+++ modules/cronpuppet/manifests/puppetfile.pp
@@ -3,6 +3,6 @@ class cronpuppet::puppetfile {
         ensure  => present,
         command => "[ -f /etc/puppetlabs/code/environments/production/Puppetfile ] && cd /etc/puppetlabs/code/environments/production && /opt/puppetlabs/bin/r10k puppetfile install",
         user    => root,
-        minute  => [1, 11, 31, 41, 51],
+        minute  => [3, 13, 33, 43, 53],
     }
 }
