28ad3f664aed57846f012ff7b567c9ce8b0eea70
Stan <s_grigorov@yahoo.com>
2018-02-23 08:31:20 -0700

    Update puppetfile.pp

--- modules/cronpuppet/manifests/puppetfile.pp
+++ modules/cronpuppet/manifests/puppetfile.pp
@@ -1,7 +1,7 @@
 class cronpuppet::puppetfile {
     cron { 'r10k-puppetfile':
         ensure  => present,
-        command => "/opt/puppetlabs/bin/r10k puppetfile install
+        command => "$ [ -f /etc/puppetlabs/code/environments/production/Puppetfile ] && [cd /etc/puppetlabs/code/environments/production] && [/opt/puppetlabs/bin/r10k puppetfile install]"
         user    => root,
         minute  => [1, 11, 31, 41, 51],
     }
