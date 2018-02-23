afcefdd09e6a79fd1c3753c77b1fc3766c884d55
Stan <s_grigorov@yahoo.com>
2018-02-23 11:35:19 -0700

    Update puppetfile.pp

--- modules/cronpuppet/manifests/puppetfile.pp
+++ modules/cronpuppet/manifests/puppetfile.pp
@@ -1,14 +1,13 @@
 class cronpuppet::puppetfile {
-#    if [ ! -f /etc/puppetlabs/code/Puppetfile ] {
-#        file { '/etc/puppetlabs/code/Puppetfile':
-#            source  => '/etc/puppetlabs/code/environments/production/files/Puppetfile',
-#        }
-#    }
-   exec { 'cronpuppet_puppetfile':
-   command => "cp /etc/puppetlabs/code/environments/production/files/Puppetfile /etc/puppetlabs/code/Puppetfile", # Double quotes for Variable interpolation
-   path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
-   unless => 'test -e /etc/puppetlabs/code/Puppetfile',
-   }
+    file { '/etc/puppetlabs/code/Puppetfile':
+        source    => '/etc/puppetlabs/code/environments/production/files/Puppetfile',
+        replace   => false,
+    }
+#   exec { 'cronpuppet_puppetfile':
+#   command => "cp /etc/puppetlabs/code/environments/production/files/Puppetfile /etc/puppetlabs/code/Puppetfile", # Double quotes for Variable interpolation
+#   path   => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:",
+#   unless => 'test -e /etc/puppetlabs/code/Puppetfile',
+#   }
     cron { 'r10k-puppetfile':
         ensure  => present,
         command => "[ -f /etc/puppetlabs/code/Puppetfile ] && cd /etc/puppetlabs/code && /opt/puppetlabs/bin/r10k puppetfile install",
