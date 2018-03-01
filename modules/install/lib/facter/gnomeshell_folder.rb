0a6634027f035946ca8e3cb84d5e6f249a92d945
Stan <s_grigorov@yahoo.com>
2018-02-28 20:03:03 -0700

    Update gnomeshell_folder.rb

--- modules/install/lib/facter/gnomeshell_folder.rb
+++ modules/install/lib/facter/gnomeshell_folder.rb
@@ -1,3 +1,20 @@
 #test
 #USER_PATH="$HOME/.local/share/gnome-shell/extensions"
 #[ -f /etc/debian_version ] && SYSTEM_PATH="/usr/local/share/gnome-shell/extensions" || SYSTEM_PATH="/usr/share/gnome-shell/extensions"
+# Return the UUID of the partition holding the /boot directory
+Facter.add('boot_dir_uuid') do
+  confine :kernel => 'Linux'
+
+  setcode do
+    df_cmd = Facter::Util::Resolution.which('df')
+    blkid_cmd = Facter::Util::Resolution.which('blkid')
+
+    partition = Facter::Core::Execution.exec("#{df_cmd} -P /boot").strip.split("\n").last.split(' ').first
+
+    uuid = Facter::Core::Execution.exec("#{blkid_cmd} -s UUID -o value #{partition}").strip
+
+    uuid = nil if (uuid.nil? || uuid.empty?)
+
+    uuid
+  end
+end
