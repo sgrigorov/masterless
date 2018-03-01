e1894d9144e1ea5c2404d3725e184628f286c8eb
Stan <s_grigorov@yahoo.com>
2018-02-28 19:54:58 -0700

    Update gdm_version.rb

--- modules/install/lib/facter/gdm_version.rb
+++ modules/install/lib/facter/gdm_version.rb
@@ -4,7 +4,7 @@
 # This is useful for applying the correct configuration file syntax.
 #
 Facter.add("gnomeshell_version") do
-  confine { File.exist?('/usr/sbin/gdm') && File.executable?("/usr/sbin/gdm") }
+  confine { File.exist?('/usr/bin/gnome-shell') && File.executable?("/usr/bin/gnome-shell") }
   setcode do
     %x{/usr/bin/gnome-shell --version}.chomp.split(/\s+/).last.match(/\d\.\d+/)
   end
