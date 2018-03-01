f08b32bdfc5667de4f9a8dc6d4282b28e674f79e
Stan <s_grigorov@yahoo.com>
2018-02-28 21:07:25 -0700

    Update gnomeshell_folder.rb

--- modules/install/lib/facter/gnomeshell_folder.rb
+++ modules/install/lib/facter/gnomeshell_folder.rb
@@ -3,7 +3,7 @@
 #[ -f /etc/debian_version ] && SYSTEM_PATH="/usr/local/share/gnome-shell/extensions" || SYSTEM_PATH="/usr/share/gnome-shell/extensions"
 # Return the UUID of the partition holding the /boot directory
 Facter.add('gnomeshell_folder') do
-  confine :kernel => 'Linux' &&  (Folder.exist?('/usr/local/share/gnome-shell/extensions') || Folder.exist?("/usr/share/gnome-shell/extensions") )
+  confine :kernel => 'Linux' &&  (File.exist?('/usr/local/share/gnome-shell/extensions') || File.exist?("/usr/share/gnome-shell/extensions") )
   setcode do
     folder = case Facter.value(:osfamily)
       when "RedHat" then "/usr/share/gnome-shell/extensions"
