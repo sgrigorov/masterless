a68564c37a1d97ba6accbbb46ef43ec6f7301e50
Stan <s_grigorov@yahoo.com>
2018-02-28 22:39:14 -0700

    Update gnomeshell_folder.rb

--- modules/install/lib/facter/gnomeshell_folder.rb
+++ modules/install/lib/facter/gnomeshell_folder.rb
@@ -6,7 +6,10 @@ Facter.add('gnomeshell_folder') do
   confine :kernel => 'Linux'
   setcode do
     folder = case Facter.value(:osfamily)
-      when "RedHat" then "/usr/share/gnome-shell/extensions"
+      when "RedHat" then
+        if File.directory? "/usr/share/gnome-shell/extensions"
+          "/usr/share/gnome-shell/extensions"
+        end
       when "Debian" then
         if File.directory? "/usr/local/share/gnome-shell/extensions"
           "/usr/local/share/gnome-shell/extensions"
