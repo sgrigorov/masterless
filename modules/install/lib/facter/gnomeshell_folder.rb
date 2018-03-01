52f4071671c71d2e1f206e16604fef780c691071
Stan <s_grigorov@yahoo.com>
2018-02-28 19:58:37 -0700

    Update gnomeshell_folder.rb

--- modules/install/lib/facter/gnomeshell_folder.rb
+++ modules/install/lib/facter/gnomeshell_folder.rb
@@ -1,1 +1,3 @@
 #test
+#USER_PATH="$HOME/.local/share/gnome-shell/extensions"
+#[ -f /etc/debian_version ] && SYSTEM_PATH="/usr/local/share/gnome-shell/extensions" || SYSTEM_PATH="/usr/share/gnome-shell/extensions"
