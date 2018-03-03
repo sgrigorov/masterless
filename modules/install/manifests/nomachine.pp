b74607e7bc03f837893ea28d0d0ae7667af6317d
Stan <s_grigorov@yahoo.com>
2018-03-03 14:23:10 -0700

    Update nomachine.pp

--- modules/install/manifests/nomachine.pp
+++ modules/install/manifests/nomachine.pp
@@ -7,23 +7,25 @@ class install::nomachine {
   $version = '6.0.78_1_x86_64'
 #)
   if $facts['nomachine_installed'] {
-    case $facts['osfamily'] {
-      'RedHat': {
-        package { 'NoMachine':
-          name     => 'nomachine_${version}',
-          ensure   => 'present',
-          provider => 'rpm',
-          source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_${version}.rpm"
+      Notify " Nomachine ${version} is already installed"
+    } else {
+      case $facts['osfamily'] {
+        'RedHat': {
+          package { 'NoMachine':
+            name     => 'nomachine_${version}',
+            ensure   => 'present',
+            provider => 'rpm',
+            source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_${version}.rpm"
+          }
         }
-      }
-      'Debian': {
-        package { 'NoMachine':
-          name     => 'nomachine_${version}',
-          ensure   => 'present',
-          provider => 'dpkg',
-          source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_${version}.deb"
+        'Debian': {
+          package { 'NoMachine':
+            name     => 'nomachine_${version}',
+            ensure   => 'present',
+            provider => 'dpkg',
+            source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_${version}.deb"
+          }
         }
       }
     }
-  }
 }
