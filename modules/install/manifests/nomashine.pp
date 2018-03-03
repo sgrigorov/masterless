66af15c10cc084cc5b78962a82cf03b672d7a007
Stan <s_grigorov@yahoo.com>
2018-03-03 00:16:11 -0700

    Update nomashine.pp

--- modules/install/manifests/nomashine.pp
+++ modules/install/manifests/nomashine.pp
@@ -3,20 +3,20 @@
 # Installs Media applications
 #
 class install::nomachine {
-  case $facts['osfamily']
-    when "RedHat" then
+  case $facts['osfamily'] {
+    'RedHat': {
       package { 'NoMachine':
       ensure   => 'installed',
       provider => 'rpm',
       source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_6.0.78_1_x86_64.rpm"
       }
-    end
-    when "Debian" then
+    }
+    'Debian': {
       package { 'NoMachine':
       ensure   => 'installed',
       provider => 'dpkg',
       source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_6.0.78_1_x86_64.deb"
       }
-    end
-  end
+    }
+  }
 }
