9365535a76c94a678e6b4a10cfd40663ab3fbf45
Stan <s_grigorov@yahoo.com>
2018-03-02 23:53:40 -0700

    Update nomashine.pp

--- modules/install/manifests/nomashine.pp
+++ modules/install/manifests/nomashine.pp
@@ -1,1 +1,10 @@
 # http://download.nomachine.com/download/6.0/Linux/nomachine_6.0.78_1_x86_64.rpm
+
+# Installs Media applications
+#
+class install::nomachine {
+  # MyPaint - similar to Microsoft Windows Paint
+  package { "mypaint":  ensure => present, }
+  # GIMP
+  package { "gimp":     ensure => present, }
+}
