6ed2d8f3bfe13bc41fa966854f2ac2d0c2b6320c
Stan <s_grigorov@yahoo.com>
2018-03-03 13:54:10 -0700

    Update nomachine_installed.rb

--- modules/install/lib/facter/nomachine_installed.rb
+++ modules/install/lib/facter/nomachine_installed.rb
@@ -1,5 +1,5 @@
 Facter.add('nomachine_installed') do
-  confine { :kernel => 'Linux' && File.exist?('/usr/NX/bin/nxserver') && File.executable?("/usr/NX/bin/nxserver") }
+  confine { File.exist?('/usr/NX/bin/nxserver') && File.executable?("/usr/NX/bin/nxserver") }
   setcode do
     %x{/usr/NX/bin/nxserver}.chomp.split(/\s+/).last
   end
