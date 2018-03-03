67ec52b54dca3a06ab4bacb573d609f8baf616ad
Stan <s_grigorov@yahoo.com>
2018-03-03 00:00:37 -0700

    Update nomashine.pp

--- modules/install/manifests/nomashine.pp
+++ modules/install/manifests/nomashine.pp
@@ -6,6 +6,6 @@ class install::nomachine {
   package { 'NoMachine':
     ensure   => 'installed',
     provider => 'rpm',
-    source   => "hhttp://download.nomachine.com/download/6.0/Linux/nomachine_6.0.78_1_x86_64.rpm"
+    source   => "http://download.nomachine.com/download/6.0/Linux/nomachine_6.0.78_1_x86_64.rpm"
   }
 }
