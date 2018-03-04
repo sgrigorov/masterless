6bfe715915965f897deaac75fc386c9defde1500
Stan <s_grigorov@yahoo.com>
2018-03-04 14:51:38 -0700

    Update puppetfile.pp

--- modules/install/manifests/puppetfile.pp
+++ modules/install/manifests/puppetfile.pp
@@ -14,13 +14,13 @@
 class install::puppetfile::r10k {
   notify {"Message from Puppetfile - r10k":}
   $pupetfile_str = "# Install puppetlabs-stdlib from the Forge
-                    # !!!! Puppetfile doesn't install the dependencies
-                    # !!!! Puppetfile installs only listed modules
-                    moduledir '/etc/puppetlabs/code/modules'
-                    mod 'puppetlabs/stdlib'
-                    #mod 'puppetlabs/apache', :latest
-                    #mod 'puppetlabs/apt'
-                    "
+# !!!! Puppetfile doesn't install the dependencies
+# !!!! Puppetfile installs only listed modules
+moduledir '/etc/puppetlabs/code/modules'
+mod 'puppetlabs/stdlib'
+
+# Added modules
+"
   file { '/etc/puppetlabs/code/Puppetfile':
       #source    => '/etc/puppetlabs/code/environments/production/files/Puppetfile',
       content => $pupetfile_str,
