6ddadfe9fa40018914149fca34f8d9b2ef5f1889
Stan <s_grigorov@yahoo.com>
2018-03-03 18:59:50 -0700

    Update pdftools.pp

--- modules/install/manifests/pdftools.pp
+++ modules/install/manifests/pdftools.pp
@@ -2,22 +2,33 @@
 # Installs Master PDF Editor
 #
 class install::pdftools {
-  $osrelease = $facts['os']['release']['full']
-  $osname = $facts['os']['name']
   # Master PDF Editor
-  # if fedora
-  package { "master-pdf-fedora":
-    name      => "master-pdf-editor-4.3.82_qt5.x86_64",
-    provider  => 'rpm',
-    ensure    => present,
-    source    => "http://get.code-industry.net/public/master-pdf-editor-4.3.82_qt5.x86_64.rpm",
-  }
-  # if ubuntu
-  #package { "master-pdf-fedora":
-    #name      => "master-pdf-editor-4.3.82_qt5.x86_64",
-    #provider  => 'apt',
-    #ensure    => present,
-    #source    => "http://get.code-industry.net/public/master-pdf-editor-4.3.82_qt5.amd64.deb",
-  #}
+  $version = '4.3.82'
+  $version_installed = $facts['nomachine_installed']
+  if $facts['masterpdfeditor_installed'] {
+      notify {"master-pdf-editor is already installed":}
+    } else {
+      case $facts['osfamily'] {
+        'RedHat': {
+            package { "master-pdf-fedora":
+            name      => "master-pdf-editor-${version}_qt5",
+            provider  => 'rpm',
+            ensure    => present,
+            source    => "http://get.code-industry.net/public/master-pdf-editor-${version}_qt5.x86_64.rpm",
+          }
+        'Debian': {
+            file { 'master-pdf-fedora.deb':
+              path   => "/tmp/master-pdf-editor-${version}_qt5.amd64.deb",
+              source => [  "http://get.code-industry.net/public/master-pdf-editor-${version}_qt5.amd64.deb",  ],
+            }
+            package { 'master-pdf-fedora':
+              name     => "master-pdf-editor-${version}_qt5",
+              provider => 'dpkg',
+              ensure   => 'present',
+              source   => "/tmp/master-pdf-editor-${version}_qt5.amd64.deb",
+            }
+          }
+        }
+      }
 }
 
