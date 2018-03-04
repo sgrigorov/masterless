e9914d7bf8bdbaed53b258791ab47891d94ed373
Stan <s_grigorov@yahoo.com>
2018-03-03 17:35:11 -0700

    Update sshserver.pp

--- modules/install/manifests/sshserver.pp
+++ modules/install/manifests/sshserver.pp
@@ -6,14 +6,14 @@ class install::sshserver {
   package { "openssh-server":  ensure => present, }
   case $facts['osfamily'] {
     'RedHat': {
-        notify {"RedHat sshd":}
+ #       notify {"RedHat sshd":}
         service { 'sshd':
         ensure    => running,
         enable    => true,
       }
     }
     'Debian': {
-        notify {"Debian ssh":}
+#        notify {"Debian ssh":}
         service { 'ssh':
         provider => 'systemd',
         ensure    => running,
