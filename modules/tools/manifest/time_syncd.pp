fdfb9941742b4c5137cf8ac72c7de3842723f1d3
Stan <s_grigorov@yahoo.com>
2018-03-04 16:38:12 -0700

    Update time_syncd.pp

--- modules/tools/manifest/time_syncd.pp
+++ modules/tools/manifest/time_syncd.pp
@@ -1,7 +1,11 @@
 #ensure service is enabled and running
-
-service { 'systemd-timesyncd':
-        provider => 'systemd',
-        ensure    => running,
-        enable    => true,
-        }
+if [ -f /usr/lib/systemd/systemd-timesyncd ] {
+notify "systemd-timesyncd exist"
+} else {
+notify "systemd-timesyncd does not exist"
+}
+#service { 'systemd-timesyncd':
+#        provider => 'systemd',
+#        ensure    => running,
+#        enable    => true,
+#        }
