e56437aa705b5aed693bb6641c18c7c66bc6f6d0
Stan <s_grigorov@yahoo.com>
2018-03-04 22:50:08 -0700

    Update systemd-timesyncd.rb

--- modules/tools/lib/facter/systemd-timesyncd.rb
+++ modules/tools/lib/facter/systemd-timesyncd.rb
@@ -1,5 +1,5 @@
 Facter.add("systemd-timesyncd") do
   setcode do
-    %x{/bin/systemctl status systemd-timesyncd1}.chomp.split(/\n/)[1]
+    %x{/bin/systemctl status systemd-timesyncd1}.chomp.split(/\n/)[1].split(/:/).last
   end
 end
