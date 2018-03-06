f1840bab09dcfbb9d17be9e5eb1d6a6acef98ec1
Stan <s_grigorov@yahoo.com>
2018-03-05 22:14:31 -0700

    Update time_syncd.pp

--- modules/tools/manifest/time_syncd.pp
+++ modules/tools/manifest/time_syncd.pp
@@ -1,5 +1,5 @@
 #ensure service is enabled and running
-if $facts['ntpd'] == 'LoadState=not-found' {
+if $facts['ntpd'] != 'LoadState=loaded' {
   notify { "ntpd not found": }
   if $facts['systemd-timesyncd'] == 'LoadState=loaded' {
     notify { "systemd-timesyncd found": }
@@ -7,9 +7,10 @@ if $facts['ntpd'] == 'LoadState=not-found' {
   notify { "time sync not found": }
 } else {
   notify { "ntpd found": }
-  if $facts['systemd-timesyncd'] == 'LoadState=loaded' {
+  if $facts['systemd-timesyncd'] != 'LoadState=loaded' {
     notify { "systemd-timesyncd not found": }
   }
+  notify { "too many time syncs": }
 }
 #service { 'systemd-timesyncd':
 #        provider => 'systemd',
