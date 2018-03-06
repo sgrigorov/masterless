2d2a6d6fe893f2b9280348548e150792fbd6a050
Stan <s_grigorov@yahoo.com>
2018-03-06 11:53:20 -0700

    Update systemd-timesyncd.rb

--- modules/tools/lib/facter/systemd-timesyncd.rb
+++ modules/tools/lib/facter/systemd-timesyncd.rb
@@ -3,8 +3,9 @@ Facter.add("systemd-timesyncd") do
 #    ! (File.exist?('/usr/sbin/ntpd') || File.exist?('/usr/sbin/openntpd') || File.exist?('/usr/sbin/chronyd') || File.exist?('/usr/sbin/VBoxService'))
 #    }
   setcode do
-    if (File.exist?('/usr/sbin/ntpd') || File.exist?('/usr/sbin/openntpd') || File.exist?('/usr/sbin/chronyd') || File.exist?('/usr/sbin/VBoxService')) {
-    %x{/bin/systemctl show -p LoadState systemd-timesyncd1}.split(/\n/)[0] } else {
+    if (File.exist?('/usr/sbin/ntpd') || File.exist?('/usr/sbin/openntpd') || File.exist?('/usr/sbin/chronyd') || File.exist?('/usr/sbin/VBoxService')) 
+      %x{/bin/systemctl show -p LoadState systemd-timesyncd1}.split(/\n/)[0]
+    else
       %x{/bin/systemctl show -p LoadState systemd-timesyncd}.split(/\n/)[0] }
     end
   end
