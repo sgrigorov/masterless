c61d4aa49bc8eb88f68384e86231fd9d898768df
Stan <s_grigorov@yahoo.com>
2018-03-05 22:19:58 -0700

    Update time_syncd.pp

--- modules/tools/manifest/time_syncd.pp
+++ modules/tools/manifest/time_syncd.pp
@@ -1,26 +1,28 @@
 #ensure service is enabled and running
-if $facts['ntpd'] != 'LoadState=loaded' {
-  notify { "ntpd not found": }
-  if $facts['systemd-timesyncd'] == 'LoadState=loaded' {
-    notify { "systemd-timesyncd found": }
-    service { 'systemd-timesyncd':
-      provider => 'systemd',
-      ensure    => running,
-      enable    => true,
+class tools::time_syncd {
+  if $facts['ntpd'] != 'LoadState=loaded' {
+    notify { "ntpd not found": }
+    if $facts['systemd-timesyncd'] == 'LoadState=loaded' {
+      notify { "systemd-timesyncd found": }
+      service { 'systemd-timesyncd':
+        provider => 'systemd',
+        ensure    => running,
+        enable    => true,
+      }
     }
-  }
-  notify { "time sync not found": }
-} else {
-  notify { "ntpd found": }
-  if $facts['systemd-timesyncd'] != 'LoadState=loaded' {
-    notify { "systemd-timesyncd not found": }
-    service { 'ntpd':
-      #provider => 'systemd',
-      ensure    => running,
-      enable    => true,
+    notify { "time sync not found": }
+  } else {
+    notify { "ntpd found": }
+    if $facts['systemd-timesyncd'] != 'LoadState=loaded' {
+      notify { "systemd-timesyncd not found": }
+      service { 'ntpd':
+        #provider => 'systemd',
+        ensure    => running,
+        enable    => true,
+      }
     }
+    notify { "too many time syncs": }
   }
-  notify { "too many time syncs": }
 }
 #service { 'systemd-timesyncd':
 #        provider => 'systemd',
