adde73626dbd50ca88dff6948b8ee1d166cc0e66
Stan <s_grigorov@yahoo.com>
2018-04-28 23:32:56 -0600

    Update _chrome.pp

--- modules/install/manifests/_chrome.pp
+++ modules/install/manifests/_chrome.pp
@@ -1,1 +1,14 @@
-install google chrome
+# install google chrome stable
+
+
+class install::chrome {
+  notify {"Message from Chrome Install":}
+  $google-chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
+  file { '/etc/apt/sources.list.d/google-chrome.list':
+      content => $google-chrome_str,
+      replace   => false,
+      #notify    => Exec['r10k_puppetfile'],
+    }
+  #package { "mypaint": ensure => present, }
+}
+
