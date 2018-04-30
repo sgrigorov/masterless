f717cdbc47a517af15cab54d4339e325df74be4b
Stan <s_grigorov@yahoo.com>
2018-04-29 19:53:38 -0600

    Update chrome.pp

--- modules/install/manifests/chrome.pp
+++ modules/install/manifests/chrome.pp
@@ -3,15 +3,30 @@
 
 class install::chrome {
   notify {"Message from Chrome Install":}
-  $google_chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
-  file { '/etc/apt/sources.list.d/google-chrome.list':
-      content => $google_chrome_str,
-      replace   => false,
-      #notify    => package['google-chrome-stable'],
-    }
+  case $facts['osfamily'] {
+    'RedHat': {
+        $google_chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
+        file { '/etc/apt/sources.list.d/google-chrome.list':
+            content => $google_chrome_str,
+            replace   => false,
+        }
+        package { "google-chrome-stable": 
+            ensure => present,
+            subscribe => File['/etc/apt/sources.list.d/google-chrome.list'],
+        }
+     }
+    'Debian': {
+        $google_chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
+        file { 'chrome_repository':
+            path    =>  '/etc/apt/sources.list.d/google-chrome.list',
+            ensure  =>  present,
+            content =>  $google_chrome_str,
+            replace =>  false,
+        }
+     }
   package { "google-chrome-stable": 
       ensure => present,
-      subscribe => File['/etc/apt/sources.list.d/google-chrome.list'],
-    }
+      subscribe => File['chrome_repository'],
+  }
 }
 
