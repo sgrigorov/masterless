7c3579ad6ed52e00f3f8868c8f7c615a1ac83692
Stan <s_grigorov@yahoo.com>
2018-04-29 20:02:51 -0600

    Update chrome.pp

--- modules/install/manifests/chrome.pp
+++ modules/install/manifests/chrome.pp
@@ -5,22 +5,29 @@ class install::chrome {
   notify {"Message from Chrome Install":}
   case $facts['osfamily'] {
     'RedHat': {
-        $google_chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
-        file { '/etc/apt/sources.list.d/google-chrome.list':
-            content => $google_chrome_str,
-            replace   => false,
-        }
-        package { "google-chrome-stable": 
-            ensure => present,
-            subscribe => File['/etc/apt/sources.list.d/google-chrome.list'],
+        #[google-chrome]
+        #name=google-chrome
+        #baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+        #enabled=1
+        #gpgcheck=1
+        #gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+        file { 'chrome_repository':
+            path    =>  '/etc/yum.repos.d/google-chrome1.repo',
+            ensure  =>  present,
+            content =>  '[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+enabled=1
+gpgcheck=1
+gpgkey=https://dl.google.com/linux/linux_signing_key.pub
+',
+            replace =>  false,
         }
      }
     'Debian': {
-        #$google_chrome_str = "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main"
         file { 'chrome_repository':
             path    =>  '/etc/apt/sources.list.d/google-chrome.list',
             ensure  =>  present,
-            #content =>  $google_chrome_str,
             content =>  'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main',
             replace =>  false,
         }
