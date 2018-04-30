393bedf42bcc55b3aea57a442fcc7381aa021fe0
Stan <s_grigorov@yahoo.com>
2018-04-29 21:10:26 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -83,6 +83,7 @@ class example {
     #include install::nomachine
     #include install::pdftools
     #include puppet_test_install
+    include install::chrome
   }
   # 2CE9374JBN - fedoratest01 - 192.168.2.166
   # 2CE9374JBQ - ubuntu 192.168.2.186
