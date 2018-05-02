a870f53fcdb74973239eacd905734391bdf53c49
Stan <s_grigorov@yahoo.com>
2018-05-02 11:45:29 -0600

    Update rpmfusion.pp

--- modules/install/manifests/rpmfusion.pp
+++ modules/install/manifests/rpmfusion.pp
@@ -1,20 +1,10 @@
 class install::rpmfusion {
+    notify {"Message from rpmfusion Install":}
+    case $facts['osid'] {
+      'CentOS': {
+          notify {"Message from rpmfusion Install - CentOS":}
+      }
+      'Fedora|Chapeau': {
+            notify {"Message from rpmfusion Install - Fedora":}
+      }
 
-  $osrelease = $facts['os']['release']['full']
-
-  # Install CentOS free Rpmfusion
-  package { "centos-rpmfusion-free":
-    name      => "rpmfusion-free-release-$osrelease.noarch",
-    provider  => 'rpm',
-    ensure    => present,
-    source    => "https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$osrelease.noarch.rpm",
-  }
-
-  # Install CentOS nonfree Rpmfusion
-  package { "centos-rpmfusion-nonfree":
-    name      => "rpmfusion-nonfree-release-$osrelease.noarch",
-    provider  => 'rpm',
-    ensure    => present,
-    source    => "https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$osrelease.noarch.rpm",
-  }
-}
