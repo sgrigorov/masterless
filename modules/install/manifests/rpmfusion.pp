fbb85eace83a0bb34b18a417de86381fc8b55b7f
Stan <s_grigorov@yahoo.com>
2018-05-02 11:35:03 -0600

    Create rpmfusion.pp

---
+++ modules/install/manifests/rpmfusion.pp
@@ -0,0 +1,20 @@
+class install::rpmfusion {
+
+  $osrelease = $facts['os']['release']['full']
+
+  # Install CentOS free Rpmfusion
+  package { "centos-rpmfusion-free":
+    name      => "rpmfusion-free-release-$osrelease.noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$osrelease.noarch.rpm",
+  }
+
+  # Install CentOS nonfree Rpmfusion
+  package { "centos-rpmfusion-nonfree":
+    name      => "rpmfusion-nonfree-release-$osrelease.noarch",
+    provider  => 'rpm',
+    ensure    => present,
+    source    => "https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$osrelease.noarch.rpm",
+  }
+}
