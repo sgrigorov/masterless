da2a55ec38e11ea9ddd261a496cf0581166a93fa
Stan <s_grigorov@yahoo.com>
2018-05-02 11:23:24 -0600

    Update chrome.pp

--- modules/install/manifests/chrome.pp
+++ modules/install/manifests/chrome.pp
@@ -5,28 +5,19 @@ class install::chrome {
   notify {"Message from Chrome Install":}
   case $facts['osfamily'] {
     'RedHat': {
-        notify {"Message from Chrome Install - RedHat":}
-        #[google-chrome]
-        #name=google-chrome
-        #baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-        #enabled=1
-        #gpgcheck=1
-        #gpgkey=https://dl.google.com/linux/linux_signing_key.pub
         file { 'chrome_repository':
             path    =>  '/etc/yum.repos.d/google-chrome1.repo',
             ensure  =>  present,
             content =>  '[google-chrome]
-name=google-chrome
-baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
-enabled=1
-gpgcheck=1
-gpgkey=https://dl.google.com/linux/linux_signing_key.pub
-',
+                         name=google-chrome
+                         baseurl=http://dl.google.com/linux/chrome/rpm/stable/x86_64
+                         enabled=1
+                         gpgcheck=1
+                         gpgkey=https://dl.google.com/linux/linux_signing_key.pub',
             replace =>  false,
         }
      }
     'Debian': {
-        notify {"Message from Chrome Install - Debian":}
         file { 'chrome_repository':
             path    =>  '/etc/apt/sources.list.d/google-chrome.list',
             content =>  'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main',
@@ -48,7 +39,6 @@ gpgkey=https://dl.google.com/linux/linux_signing_key.pub
   package { "google-chrome-stable": 
       ensure => present,
       subscribe => File['chrome_repository'],
-      #require   => Exec['apt_update'],
   }
 }
 
