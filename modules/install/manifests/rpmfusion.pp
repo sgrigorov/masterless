26688de8158c143322f8e998bfe1d9f2295058a8
Stan <s_grigorov@yahoo.com>
2018-05-02 12:05:28 -0600

    Update rpmfusion.pp

--- modules/install/manifests/rpmfusion.pp
+++ modules/install/manifests/rpmfusion.pp
@@ -1,7 +1,7 @@
 class install::rpmfusion {
     $osid = $facts['os']['distro']['id']
-    #notify {"Message from rpmfusion Install $osid":}
-    notify {"M"$osid:}
+    notify {"Message from rpmfusion Install":}
+    #notify {"M"$osid:}
     case $osid {
       'CentOS': {
           notify {"Message from rpmfusion Install - CentOS":}
