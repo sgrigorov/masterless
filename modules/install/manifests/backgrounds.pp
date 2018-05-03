eb71b9fa16a06490269be1d910140c122e329e97
Stan <s_grigorov@yahoo.com>
2018-05-03 17:31:11 -0600

    Update backgrounds.pp

--- modules/install/manifests/backgrounds.pp
+++ modules/install/manifests/backgrounds.pp
@@ -1,1 +1,23 @@
 # dnf install f28-backgrounds-extras-gnome
+
+class install::backgrounds {
+    $osid = $facts['os']['distro']['id']
+    $osrelease = $facts['os']['release']['full']
+    
+    # if gnome is installed
+    
+    notify {"Message from backgrounds Install":}
+    case $osid {
+      'CentOS': {
+            # Install CentOS backgrounds
+            }
+      }
+      'Fedora', 'Chapeau': {
+            # Install Fedora backgrounds-extras-gnome
+            package { "backgrounds-extras-gnome":
+                name      => "f$osrelease-backgrounds-extras-gnome.noarch",
+                ensure    => present,
+            }
+      }
+    }
+}
