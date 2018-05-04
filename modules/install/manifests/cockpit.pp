69d8ffe7e654f41c26a5a76fc70cc5aadf6ca2cf
Stan <s_grigorov@yahoo.com>
2018-05-04 13:48:57 -0600

    Update cockpit.pp

--- modules/install/manifests/cockpit.pp
+++ modules/install/manifests/cockpit.pp
@@ -5,8 +5,14 @@ class install::cockpit {
   notify {"Message from Cockpit Install":}
   case $facts['osfamily'] {
     'RedHat': {
-        package { "cockpit":    ensure   => present, }
-     }
+        package { "cockpit":    ensure  => present, }
+        # Open port 8080
+        # firewall-cmd --permanent --add-port=8080/tcp
+        service { "cockpit":
+            ensure    => running,
+            enable    => true,
+        }
+      }
     'Debian': {
     }
   }
