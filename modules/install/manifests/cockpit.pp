c44326539e3daceb3fa9ec25ced6741732a87b23
Stan <s_grigorov@yahoo.com>
2018-05-04 13:25:45 -0600

    Update cockpit.pp

--- modules/install/manifests/cockpit.pp
+++ modules/install/manifests/cockpit.pp
@@ -5,8 +5,10 @@ class install::cockpit {
   notify {"Message from Cockpit Install":}
   case $facts['osfamily'] {
     'RedHat': {
+        notify {"Message from Cockpit Install - RedHat":}
         package { "cockpit":    ensure   => present, }
      }
     'Debian': {
+        notify {"Message from Cockpit Install - Debian":}
     }
 }
