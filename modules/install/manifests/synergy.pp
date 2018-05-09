33f0f819d9de623cef04ce09ae26834ef0cc7579
Stan <s_grigorov@yahoo.com>
2018-05-08 18:02:19 -0600

    Update and rename _synergy.pp to synergy.pp

---
+++ modules/install/manifests/synergy.pp
@@ -0,0 +1,19 @@
+# Install Synergy
+
+
+class install::synergy {
+  notify {"Message from Synergy Install":}
+  case $facts['osfamily'] {
+    'RedHat': {
+        package { "synergy":    ensure  => present, }
+        # Open port 8080
+        # firewall-cmd --permanent --add-port=8080/tcp
+        #service { "cockpit":
+        #    ensure    => running,
+        #    enable    => true,
+        }
+      }
+    'Debian': {
+    }
+  }
+}
