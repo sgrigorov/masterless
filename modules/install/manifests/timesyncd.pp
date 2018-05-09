0e2ae8c151fcae44451f53b3139c3c7dc5931dd5
Stan <s_grigorov@yahoo.com>
2018-05-08 19:26:25 -0600

    Update timesyncd.pp

--- modules/install/manifests/timesyncd.pp
+++ modules/install/manifests/timesyncd.pp
@@ -15,11 +15,25 @@ class install::timesyncd {
       }
     }
     'Fedora': {
-      service { 'systemd-timesyncd':
-        provider => 'systemd',
-        ensure    => running,
-        enable    => true,
-      }
+      case $facts['os']['release']['full'] {
+        '28': {
+            package { "chrony":  ensure => present, }
+            service { 'ntpd':
+              ensure    => stopped,
+              enable    => false,
+            }
+            service { 'chronyd':
+              ensure    => running,
+              enable    => true,
+            }
+        }
+        default: {
+            service { 'systemd-timesyncd':
+              provider => 'systemd',
+              ensure    => running,
+              enable    => true,
+            }
+        }
     }
   }
 }
