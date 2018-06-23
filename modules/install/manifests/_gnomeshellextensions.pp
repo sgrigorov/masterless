a7e8638995cebdc9216ea9d38117a120bd67331a
Stan <s_grigorov@yahoo.com>
2018-06-23 13:47:59 -0600

    Update _gnomeshellextensions.pp

--- modules/install/manifests/_gnomeshellextensions.pp
+++ modules/install/manifests/_gnomeshellextensions.pp
@@ -1,6 +1,8 @@
 #https://github.com/brunelli/gnome-shell-extension-installer/blob/master/gnome-shell-extension-installer
 #http://bernaerts.dyndns.org/linux/76-gnome/283-gnome-shell-install-extension-command-line-script
 #http://bernaerts.dyndns.org/linux/76-gnome/345-gnome-shell-install-remove-extension-command-line-script
+#docker integration 
+#Gnome Hub
 
 ########### sudo apt install gnome-tweak-tool #############
 
