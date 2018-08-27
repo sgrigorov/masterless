f450911cbf4e3dfa6bfc1053512948867a375966
Stan <s_grigorov@yahoo.com>
2018-08-27 11:17:00 -0600

    Update tools.pp

--- modules/install/manifests/tools.pp
+++ modules/install/manifests/tools.pp
@@ -6,9 +6,12 @@ class install::tools {
   package { "unzip":            ensure => present, }
   package { "vim":              ensure => present, }
   package { "vim-powerline":    ensure => present, }
+  #add config for vimrc
   package { "tilix":            ensure => present, }
   package { "powerline":        ensure => present, }
   package { "powerline-fonts":  ensure => present, }
+  # add config for bashrc
+  # https://fedoramagazine.org/add-power-terminal-powerline/
   #policycoreutils - core utilities for SElinux - RedHat osfamily
   package { "policycoreutils":  ensure => present, }
 }
